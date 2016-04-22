f6d49d302c99c68b4afbabc7af8ae4253c00f46b
WangJace <wang-jace@foxmail.com>
2016-04-22 22:30:32 +0800

    first commit

--- RainbowGauge/RainbowView.h
+++ RainbowGauge/RainbowView.h
@@ -8,10 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-#define degressToRadius(ang) (M_PI*(ang)/180.0f) //把角度转换成PI的方式
-#define PROGRESS_WIDTH 80 // 圆直径
-#define PROGRESS_LINE_WIDTH 6 //弧线的宽度
-
+IB_DESIGNABLE
 @interface RainbowView : UIView
+/**
+ *  三角形的边长
+ */
+@property (nonatomic, assign) IBInspectable CGFloat triangleSide;
+/**
+ *  彩虹半圆线宽
+ */
+@property (nonatomic, assign) IBInspectable CGFloat rainbowLineWidth;
+/**
+ *  数字Label的线宽
+ */
+@property (nonatomic, assign) IBInspectable CGFloat numberLabelWidth;
+
+/**
+ *  设置numberLabel的数值
+ *
+ *  @param number 数值
+ */
+- (void)setNumber:(NSInteger)number;
 
 @end
--- RainbowGauge/RainbowView.m
+++ RainbowGauge/RainbowView.m
@@ -7,23 +7,48 @@
 //
 
 #import "RainbowView.h"
+#import "Triangle.h"
 
+#define DegressToRadius(ang) (M_PI*(ang)/180.0f) //把角度转换成PI的方式
 #define RYUIColorWithRGB(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
+#define WIDTH CGRectGetWidth(self.frame)
+#define HEIGHT CGRectGetHeight(self.frame)
+#define PERSTRESSNEMBERDURATION 0.01
 
 @interface RainbowView ()
 {
     CAShapeLayer * _progressLayer;
+    CALayer *_grain;
+    CAGradientLayer *_gradientLayer;
+    CGFloat _radius;
     
+    CGFloat _preAngle;
+    CGFloat _preNumber;
+    CGPoint _circleCenter;
 }
+
+@property (nonatomic, strong) UILabel *numberLabel;
+@property (nonatomic, strong) Triangle *triangle;
+
 @end
 
 @implementation RainbowView
+
+- (instancetype)init
+{
+    if ((self = [super init])) {
+        self.translatesAutoresizingMaskIntoConstraints = NO;
+        [self setSubView];
+    }
+    return self;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self gradentWith:frame];    }
+        [self setSubView];
+    }
     return self;
 }
 
@@ -31,64 +56,123 @@
 {
     self = [super initWithCoder:coder];
     if (self) {
-        [self gradentWith];
-        
+        [self setSubView];
     }
     return self;
 }
 
-- (void)gradentWith{
-    NSLog(@"=-----%f",CGRectGetMidX(self.frame));
-    
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    [self updateUI];
 }
 
-- (void)gradentWith:(CGRect)frame{
-    //设置贝塞尔曲线
+- (void)setSubView
+{
+    _triangleSide = 10;
+    _numberLabelWidth = 30;
+    _rainbowLineWidth = 5;
     
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2) radius:(frame.size.width-PROGRESS_LINE_WIDTH)/2-5 startAngle:degressToRadius(-180) endAngle:degressToRadius(0) clockwise:YES];
+    _preNumber = 0;
+    _preAngle = 0;
+    
+    _numberLabel = [[UILabel alloc] init];
+    _numberLabel.textAlignment = NSTextAlignmentCenter;
+    _numberLabel.text = @"0";
+    [self addSubview:_numberLabel];
+    
+    _triangle = [[Triangle alloc] init];
+    _triangle.backgroundColor = [UIColor clearColor];
+    [self addSubview:_triangle];
     
     //遮罩层
     _progressLayer = [CAShapeLayer layer];
-    _progressLayer.frame = self.bounds;
     _progressLayer.fillColor =  [[UIColor clearColor] CGColor];
     _progressLayer.strokeColor=[UIColor redColor].CGColor;
     _progressLayer.lineCap = kCALineCapButt;
-    _progressLayer.lineWidth = PROGRESS_LINE_WIDTH+5;
-    _progressLayer.path=path.CGPath;
     
     //渐变图层
-    CALayer * grain = [CALayer layer];
-    CAGradientLayer *gradientLayer =  [CAGradientLayer layer];
-    gradientLayer.frame = CGRectMake(0, 0, self.bounds.size.width-0, self.bounds.size.height-0);
-    [gradientLayer setColors:[NSArray arrayWithObjects:(id)[RYUIColorWithRGB(39, 75, 174) CGColor],(id)[RYUIColorWithRGB(75, 128, 107) CGColor], (id)[RYUIColorWithRGB(157, 202, 44) CGColor], (id)[RYUIColorWithRGB(231, 195, 43) CGColor], (id)[RYUIColorWithRGB(218, 19, 22) CGColor],nil]];
-    [gradientLayer setLocations:@[@0,@0.25,@0.5,@0.75,@1]];
-    [gradientLayer setStartPoint:CGPointMake(0, 0)];
-    [gradientLayer setEndPoint:CGPointMake(1, 0)];
-    [grain addSublayer:gradientLayer];
+    _grain = [CALayer layer];
+    _gradientLayer =  [CAGradientLayer layer];
+    [_gradientLayer setColors:[NSArray arrayWithObjects:(id)[RYUIColorWithRGB(39, 75, 174) CGColor],(id)[RYUIColorWithRGB(75, 128, 107) CGColor], (id)[RYUIColorWithRGB(157, 202, 44) CGColor], (id)[RYUIColorWithRGB(231, 195, 43) CGColor], (id)[RYUIColorWithRGB(218, 19, 22) CGColor],nil]];
+    [_gradientLayer setLocations:@[@0,@0.25,@0.5,@0.75,@1]];
+    [_gradientLayer setStartPoint:CGPointMake(0, 0)];
+    [_gradientLayer setEndPoint:CGPointMake(1, 0)];
+    [_grain addSublayer:_gradientLayer];
     
     //用progressLayer来截取渐变层 遮罩
-    [grain setMask:_progressLayer];
-    [self.layer addSublayer:grain];
-    
-    
-    //增加动画
-    
-    CABasicAnimation *pathAnimation=[CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    
-    pathAnimation.duration = 6;
-    
-    pathAnimation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    
-    pathAnimation.fromValue=[NSNumber numberWithFloat:0.0f];
-    
-    pathAnimation.toValue=[NSNumber numberWithFloat:1.0f];
-    
-    pathAnimation.autoreverses=NO;
-    pathAnimation.repeatCount = INFINITY;
-    //    [_progressLayer addAnimation:pathAnimation forKey:@"strokeEndAnimation"];
-    
-    
+    [_grain setMask:_progressLayer];
+    [self.layer addSublayer:_grain];
 }
+
+- (void)updateUI
+{
+    if (WIDTH/2.0 > HEIGHT) {
+        _radius = HEIGHT-_triangleSide*cos(M_PI/6)-_numberLabelWidth-_rainbowLineWidth;
+    }
+    else {
+        _radius = WIDTH/2.0-_triangleSide*cos(M_PI/6)-_numberLabelWidth-_rainbowLineWidth;
+    }
+    
+    _circleCenter = CGPointMake(WIDTH/2.0, HEIGHT);
+    
+    CGFloat x = WIDTH/2.0 - _radius - _rainbowLineWidth - _triangleSide*cos(M_PI/6) - _numberLabelWidth;
+    _numberLabel.frame = CGRectMake(x, HEIGHT-_numberLabelWidth/2.0, _numberLabelWidth, _numberLabelWidth);
+    _triangle.frame = CGRectMake(x+_numberLabelWidth, HEIGHT-_triangleSide/2.0, _triangleSide*cos(M_PI/6), _triangleSide);
+    
+    //设置贝塞尔曲线
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:_circleCenter radius:_radius startAngle:DegressToRadius(-180) endAngle:DegressToRadius(0) clockwise:YES];
+    _progressLayer.frame = self.bounds;
+    _progressLayer.lineWidth = _rainbowLineWidth*2;
+    _progressLayer.path=path.CGPath;
+    
+    _gradientLayer.frame = CGRectMake(WIDTH/2.0-_radius-_rainbowLineWidth, 0, (_radius+_rainbowLineWidth)*2, HEIGHT);
+}
+
+- (void)setNumber:(NSInteger)number
+{
+    _numberLabel.text = [NSString stringWithFormat:@"%ld",(long)number];
+    CGFloat angle = (number/100.0)*M_PI;
+    CGFloat triangleRadius = _radius+_rainbowLineWidth+_triangleSide*cos(M_PI/6)/2.0;
+    CGFloat numberLabelRadius = triangleRadius+_numberLabelWidth/2.0+5;
+    
+    CAAnimationGroup *triangleAnimation = [CAAnimationGroup animation];
+    
+    CAKeyframeAnimation *trianglePointAnimation = [CAKeyframeAnimation animation];
+    trianglePointAnimation.keyPath = @"position";
+    if (_preAngle > angle) {
+        trianglePointAnimation.path = [UIBezierPath bezierPathWithArcCenter:_circleCenter radius:triangleRadius startAngle:_preAngle-DegressToRadius(180) endAngle:angle-DegressToRadius(180) clockwise:NO].CGPath;
+    }
+    else {
+        trianglePointAnimation.path = [UIBezierPath bezierPathWithArcCenter:_circleCenter radius:triangleRadius startAngle:_preAngle-DegressToRadius(180) endAngle:angle-DegressToRadius(180) clockwise:YES].CGPath;
+    }
+    
+    CAKeyframeAnimation *triangleRotationAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation"];
+    triangleRotationAnimation.values = @[[NSNumber numberWithFloat:_preAngle], [NSNumber numberWithFloat:angle]];
+    
+    [triangleAnimation setAnimations:@[trianglePointAnimation,triangleRotationAnimation]];
+    triangleAnimation.duration = fabs(number - _preNumber)*PERSTRESSNEMBERDURATION;
+    triangleAnimation.removedOnCompletion = NO;
+    triangleAnimation.fillMode = kCAFillModeForwards;
+    [_triangle.layer addAnimation:triangleAnimation forKey:nil];
+    
+    CAKeyframeAnimation *numberLabelAnimation = [CAKeyframeAnimation animation];
+    numberLabelAnimation.keyPath = @"position";
+    numberLabelAnimation.duration = fabs(number -_preNumber)*PERSTRESSNEMBERDURATION;
+    if (_preAngle > angle) {
+        numberLabelAnimation.path = [UIBezierPath bezierPathWithArcCenter:_circleCenter radius:numberLabelRadius startAngle:_preAngle-DegressToRadius(180) endAngle:angle-DegressToRadius(180) clockwise:NO].CGPath;
+    }
+    else {
+        numberLabelAnimation.path = [UIBezierPath bezierPathWithArcCenter:_circleCenter radius:numberLabelRadius startAngle:_preAngle-DegressToRadius(180) endAngle:angle-DegressToRadius(180) clockwise:YES].CGPath;
+    }
+    numberLabelAnimation.removedOnCompletion = NO;
+    numberLabelAnimation.fillMode = kCAFillModeForwards;
+    [_numberLabel.layer addAnimation:numberLabelAnimation forKey:nil];
+    
+    _preAngle = angle;
+    _preNumber = number;
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
